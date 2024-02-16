df63146801434184249fac3ec71aba3b7fc01cdd
Jakub Jackowski <jjackowski.ext@eurofunk.com>
2024-02-16 18:32:45 +0100

    Connect and use mysql db

---
+++ src/main/resources/db/migration/V1__init-mysql-database.sql
@@ -0,0 +1,47 @@
+    drop table if exists beer;
+
+    drop table if exists customer;
+
+    create table beer (
+                          beer_style tinyint,
+                          price decimal(38,2),
+                          quantity_on_hand integer,
+                          version integer,
+                          created_date datetime(6),
+                          updated_date datetime(6),
+                          id varchar(36) not null,
+                          beer_name varchar(50),
+                          upc varchar(255),
+                          primary key (id)
+    ) engine=InnoDB;
+
+    create table customer (
+                              version integer,
+                              created_date datetime(6),
+                              last_modified_date datetime(6),
+                              id varchar(36) not null,
+                              customer_name varchar(255),
+                              primary key (id)
+    ) engine=InnoDB;
+
+    create table beer (
+                          beer_style tinyint,
+                          price decimal(38,2),
+                          quantity_on_hand integer,
+                          version integer,
+                          created_date datetime(6),
+                          updated_date datetime(6),
+                          id varchar(36) not null,
+                          beer_name varchar(50),
+                          upc varchar(255),
+                          primary key (id)
+    ) engine=InnoDB;
+
+    create table customer (
+                              version integer,
+                              created_date datetime(6),
+                              last_modified_date datetime(6),
+                              id varchar(36) not null,
+                              customer_name varchar(255),
+                              primary key (id)
+    ) engine=InnoDB;
