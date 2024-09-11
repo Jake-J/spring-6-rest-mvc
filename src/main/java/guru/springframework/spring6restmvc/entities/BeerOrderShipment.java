52127ae0bc08ce16612868c2560372cb9199ca5a
Jakub Jackowski <jjackowski.ext@eurofunk.com>
2024-09-11 13:49:50 +0200

    Add new migration files with beer orders, category, beer order shipments Create relations between new entities Handle creating relations between entites

--- src/main/java/guru/springframework/spring6restmvc/entities/BeerOrderShipment.java
+++ src/main/java/guru/springframework/spring6restmvc/entities/BeerOrderShipment.java
@@ -2,41 +2,44 @@ package guru.springframework.spring6restmvc.entities;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.type.SqlTypes;
 
-import java.time.LocalDateTime;
-import java.util.HashSet;
-import java.util.Set;
+import java.sql.Timestamp;
 import java.util.UUID;
 
 @Getter
 @Setter
-@Builder
-@Data
-@Entity
-@AllArgsConstructor
 @NoArgsConstructor
-public class Customer {
-    
+@AllArgsConstructor
+@Entity
+@Builder
+public class BeerOrderShipment {
     @Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(name="UUID", strategy = "org.hibernate.id.UUIDGenerator")
     @JdbcTypeCode(SqlTypes.CHAR)
     @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
     private UUID id;
-    private String customerName;
-
-    @Column(length = 255)
-    private String email;
 
     @Version
-    private Integer version;
-    private LocalDateTime createdDate;
-    private LocalDateTime  lastModifiedDate;
+    private Long version;
 
-    @Builder.Default
-    @OneToMany(mappedBy = "customer")
-    private Set<BeerOrder> beerOrders = new HashSet<>();
+    private String trackingNumber;
+
+    @OneToOne
+    private BeerOrder beerOrder;
+
+    @Override
+    public int hashCode() { return  super.hashCode(); }
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdDate;
+
+    @UpdateTimestamp
+    private Timestamp lastModifiedDate;
 }
--- src/main/resources/db/migration/V1__init-mysql-database.sql
+++ src/main/resources/db/migration/V1__init-mysql-database.sql
@@ -13,7 +13,7 @@
                           beer_name varchar(50),
                           upc varchar(255),
                           primary key (id)
-    ) engine=InnoDB;
+    );
 
     create table customer (
                               version integer,
@@ -22,26 +22,4 @@
                               id varchar(36) not null,
                               customer_name varchar(255),
                               primary key (id)
-    ) engine=InnoDB;
-
-    create table beer (
-                          beer_style tinyint,
-                          price decimal(38,2),
-                          quantity_on_hand integer,
-                          version integer,
-                          created_date datetime(6),
-                          updated_date datetime(6),
-                          id varchar(36) not null,
-                          beer_name varchar(50),
-                          upc varchar(255),
-                          primary key (id)
-    ) engine=InnoDB;
-
-    create table customer (
-                              version integer,
-                              created_date datetime(6),
-                              last_modified_date datetime(6),
-                              id varchar(36) not null,
-                              customer_name varchar(255),
-                              primary key (id)
-    ) engine=InnoDB;
+    );
